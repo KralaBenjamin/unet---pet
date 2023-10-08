62fc00f982a1b2057cfad24e433ade2f4408cabd
kralabenjamin <kralabenjamin@gmail.com>
2023-10-08 17:47:42 +0200

    init unet stuff

---
+++ dataset_pet.py
@@ -0,0 +1,99 @@
+# lade Bilder und wandel sie in NP Arrays um
+
+# https://academictorrents.com/details/b18bbd9ba03d50b0f7f479acc9f4228a408cecc1
+
+
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+import numpy as np
+from os import listdir
+from os.path import join
+from random import random
+import torch
+from einops import rearrange
+
+
+class PetOnlySegmentationDataSet(Dataset):
+    def __init__(self, ImageFileList, SegmentationFileList, transform=None):
+        self.imagefilelist = ImageFileList
+        self.segmentationfilelist = SegmentationFileList
+        self.transform = transform  
+
+    def __getitem__(self, index):
+        image_file = self.imagefilelist[index]
+        seg_file = self.segmentationfilelist[index]
+
+        image = np.array(Image.open(image_file).convert('RGB'))
+        segmentation = np.array(Image.open(seg_file))
+
+        # siehe Pet Readme, beides Hintergrund
+        segmentation[segmentation == 2] = 0
+        segmentation[segmentation == 3] = 0
+
+        if self.transform is not None:
+            pass
+
+        image = torch.tensor(image)
+        image = rearrange(image, "H W C -> C H W")
+        image = image.unsqueeze(dim=0).float()
+
+        segmentation =  torch.tensor(segmentation).float()
+
+        return image, segmentation
+    
+    def __len__(self):
+        return len(self.imagefilelist)
+
+def get_train_val_file_list(split=0.7):
+    image_path = 'pet_data/images'
+    seg_path = 'pet_data/annotations/trimaps'
+    image_filelist = sorted([filename
+        for filename in listdir(image_path)
+        if not filename.endswith('.mat')
+    ])
+    seg_file_list = [filename
+        for filename in listdir(seg_path)
+        if not filename.startswith('._')
+    ]
+    seg_file_list = sorted(seg_file_list)
+
+    train_image_list = list()
+    train_seg_list = list()
+
+    val_image_list = list()
+    val_seg_list = list()
+
+
+    for image_file, seg_file in zip(image_filelist, seg_file_list):
+        long_image_file = join(image_path, image_file)
+        long_seg_file = join(seg_path, seg_file)
+
+        if random() <= split:
+            train_image_list.append(long_image_file)
+            train_seg_list.append(long_seg_file)
+        else:
+            val_image_list.append(long_image_file)
+            val_seg_list.append(long_seg_file)
+
+    return (train_image_list, train_seg_list), (val_image_list, val_seg_list)
+    
+
+if __name__ == "__main__":
+    (train_image_list, train_seg_list), (val_image_list, val_seg_list) = get_train_val_file_list()
+    breakpoint()
+
+    train_dataset = PetOnlySegmentationDataSet(train_image_list, train_seg_list)
+    train_dl = DataLoader(train_dataset)
+
+
+    val_dataset = PetOnlySegmentationDataSet(val_image_list, val_seg_list)
+    val_dl = DataLoader(val_dataset)
+
+    print("trainign..")
+    #for x, y in train_dl:
+        #print(x.shape, y.shape)
+        #pass
+
+    print('validation...')
+    for x, y in val_dl:
+        print(x.shape, y.shape)
---
+++ training_unet_only_pet_segmentation.py
@@ -0,0 +1,80 @@
+# Augmentation Random Horizontal Flip? Resize, Rotation, Normalize, <= nachdem Training steht
+# Welche verwende ich bei Bonirop? Color Jitter? 
+
+from unet import UNET
+from dataset_pet import get_train_val_file_list, PetOnlySegmentationDataSet
+import torch
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+def main():
+    DEVICE = 'cpu'
+    EPOCHS = 5
+
+    model = UNET(3, 1).to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters())
+    criterion = torch.nn.BCEWithLogitsLoss()
+
+    (train_image_list, train_seg_list), (val_image_list, val_seg_list) = \
+        get_train_val_file_list()
+    
+    train_dataset = PetOnlySegmentationDataSet(train_image_list, train_seg_list)
+    train_dl = DataLoader(train_dataset, shuffle=True)
+
+    val_dataset = PetOnlySegmentationDataSet(val_image_list, val_seg_list)
+    val_dl = DataLoader(val_dataset)
+
+
+    """    
+        over_x, over_y = train_dataset[0]
+        for i in range(10):
+            print(i)
+            pred = model(over_x)
+
+            loss = criterion(pred[0, 0], over_y)
+            loss.backward()
+
+            optimizer.step()
+            optimizer.zero_grad()
+
+        pred = model(over_x)
+        pred_log = (pred > 0.5).float()
+        summe  = (pred_log == over_y).sum()
+        breakpoint()
+        print(summe / (pred.shape[0] * pred.shape[1]), pred.shape, over_y)
+        # todo: berechnung fetirg machen
+    """
+
+    """
+    TODO: UserWarning: The default value of the antialias parameter of all the 
+    resizing transforms (Resize(), RandomResizedCrop(), etc.) will change from 
+    None to True in v0.17, in order to be consistent across the PIL and Tensor 
+    backends. To suppress this warning, directly pass antialias=True (recommended, 
+    future default), antialias=None (current default, which means False for Tensors 
+    and True for PIL), or antialias=False (only works on Tensors - PIL will still use 
+    antialiasing). This also applies if you are using the inference transforms from the 
+    models weights: update the call to weights.transforms(antialias=True).
+    """
+    """
+    TODO: Croppe den Scheiß für mehr Effizienz!
+    
+    """
+
+    n_trained_batches = 0
+    for i_epoch in range(EPOCHS):
+        for batch_x, batch_y in tqdm(train_dl):
+            pred = model(batch_x[0])
+
+            loss = criterion(pred[0], batch_y)
+            loss.backward()
+
+            if n_trained_batches >= 128:
+
+                optimizer.step()
+                optimizer.zero_grad()
+            else:
+                n_trained_batches += 1
+
+
+if __name__ == '__main__':
+    main()
